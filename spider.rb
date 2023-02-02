44fc7dcc71534ff6d721da7f503b772c44c90629
Nicolas Rey <nic.m.rey@gmail.com>
2023-02-02 14:39:46 -0800

    Defined Results and Enqueued Urls for Spider Class

--- spider.rb
+++ spider.rb
@@ -17,7 +17,6 @@ class Spider
         enqueue(processor.root, processor.handler)
     end
 
-
     def enqueue(url, method)
         url = agent.resolve(url).to_s
         return if @handlers[url]
@@ -29,9 +28,40 @@ class Spider
         @results << data
     end
 
+    def results
+        return enum_for(:results) unless block_given?
+
+        index - @results.length
+        enqueued_urls.each do |url, handler|
+
+            # process url
+            @processor.send(handler[:method], agent.get(url), handler[:data])
+
+            if block_given? && @results.length > index
+                yield @results.last
+                index += 1
+            end
+
+            # crawl delay
+            sleep @interval if @interval > 0
+        end
+    end
+
     private
 
     def agent
         @agent ||= Mechanize.new
+    end
+
+    def enqueued_urls
+        Enumerator.new do |y|
+            index = 0
+            while index < @urls.count && index <= @max_urls
+                url = @urls[index]
+                index += 1
+                next unless url
+                y.yield url, @handlers [url]
+            end
+        end
     end
 end
