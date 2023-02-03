71d62a44a4691fc47cbd63d414be3084c8c5487b
Nicolas Rey <nic.m.rey@gmail.com>
2023-02-02 16:01:10 -0800

    Added ApiList class, crawler functions at basic level.

--- spider.rb
+++ spider.rb
@@ -17,12 +17,11 @@ class Spider
         enqueue(processor.root, processor.handler)
     end
 
-    def enqueue(url, method)
-        url = agent.resolve(url).to_s
+    def enqueue(url, method, data = {})
         return if @handlers[url]
         @urls << url
-        @handlers[url] ||= {method: method, data: {} }
-    end
+        @handlers[url] ||= { method: method, data: data }
+      end
 
     def record(data = {})
         @results << data
@@ -31,7 +30,7 @@ class Spider
     def results
         return enum_for(:results) unless block_given?
 
-        index - @results.length
+        index = @results.length
         enqueued_urls.each do |url, handler|
 
             # process url
@@ -65,3 +64,60 @@ class Spider
         end
     end
 end
+
+
+class ApiList
+    attr_reader :root, :handler
+    
+    def initialize (root: "https://apilist.fun", handler: :process_index, **options)
+        @root = root
+        @handler = handler
+        @options = options
+    end
+
+    def results(&block)
+        spider.results(&block)
+    end
+
+    def process_index(page, data = {})
+        puts "processing index..."
+        page.links_with(href: %r{\?page=\d+}).each do |link|
+            spider.enqueue(link.href, :process_index)
+        end
+
+        page.links_with(href: %r{/api/\w+$}).each do |link|
+            spider.enqueue(link.href, :process_api, name: link.text)
+        end
+    end
+
+    def process_api(page, data = {})
+        print "processing api "
+        fields = page.search("#tabs-content .field").each_with_object({}) do |tage, o|
+            key = tag.search("label").text.strip.downcase.gsub(%r{[^\w]+}, ' ').gsub(%r{\s+}, "_").to_sym
+            val = tag.search("span").text
+            o[key] = val
+        end
+
+        # categories = page.search("article.node-api .tags").first.text.strip.split(/\s+/)
+
+        spider.record data.merge(fields)#.merge(categories: categories)
+    end
+
+    
+
+    private
+
+    def spider
+        @spider ||= Spider.new(self, @options)
+    end
+
+
+end
+
+spider = ApiList.new
+
+    spider.results.lazy.take(10).each_with_index do |result, i|
+        puts "%-3s: %s" % [i, result.inspect]
+    end
+
+puts "web crawl complete"
